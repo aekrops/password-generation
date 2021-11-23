fa75f49acabf72bcfdb46d7e95e3334ef746ee62
aekrops <svyatoslavtrostynski24@gmail.com>
2021-11-23 05:19:51 +0200

    added pin&hex API

--- lib/components/api.dart
+++ lib/components/api.dart
@@ -1,21 +1,36 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:http/http.dart' as http;
 import 'dart:developer';
 
 
 
-Future<String> fetchData(length) async {
+Future<String> fetchPassword(length) async {
   final response = await http.get(
-      Uri.parse("https://random.justyy.workers.dev/api/random/?cached&n=$length"));
+      Uri.parse("https://makemeapassword.ligos.net/api/v1/pronounceable/plain?c=1&sc=$length"));
   if (response.statusCode == 200) {
-    // If the server did return a 200 OK response,
-    // then parse the JSON.
-    log(response.body);
     return response.body;
   } else {
-    // If the server did not return a 200 OK response,
-    // then throw an exception.
+    throw Exception('Failed to load album');
+  }
+}
+
+
+Future<String> fetchPINCode(length) async {
+  final response = await http.get(
+      Uri.parse("https://makemeapassword.ligos.net/api/v1/pin/plain?c=1&l=$length"));
+  if (response.statusCode == 200) {
+    return response.body;
+  } else {
+    throw Exception('Failed to load album');
+  }
+}
+
+
+Future<String> fetchHex(length) async {
+  final response = await http.get(
+      Uri.parse("https://makemeapassword.ligos.net/api/v1/hex/plain?c=1&l=$length"));
+  if (response.statusCode == 200) {
+    return response.body;
+  } else {
     throw Exception('Failed to load album');
   }
 }
